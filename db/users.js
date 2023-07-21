3ed80733699ed015d9e91ee9dde06eb1fdffbea2
Dragonsofash <agpaoajonathanb@gmail.com>
2023-07-20 20:19:47 -0400

    Passed all tests for users.js

--- db/users.js
+++ db/users.js
@@ -8,25 +8,25 @@ const SALT_COUNT = 10;
 // user functions
 async function createUser({ username, password }) {
   const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
-  let userToAdd = { username, hashedPassword };
-  try {
-    const {
-      rows: [user],
-    } = await client.query(
-      `
+
+  const {
+    rows: [user],
+  } = await client.query(
+    `
           INSERT INTO users(username, password) 
           VALUES($1, $2) 
           ON CONFLICT (username) DO NOTHING 
           RETURNING *;
         `,
-      [username, hashedPassword]
-    );
+    [username, hashedPassword]
+  );
 
-    delete user.password;
-    return userToAdd;
-  } catch (error) {
-    throw error;
+  if (!user) {
+    return null;
   }
+
+  delete user.password;
+  return user;
 }
 
 async function getUser({ username, password }) {
@@ -34,6 +34,7 @@ async function getUser({ username, password }) {
   const hashedPassword = user.password;
   // isValid will be a boolean based on wether the password matches the hashed password
   let passwordsMatch = await bcrypt.compare(password, hashedPassword);
+
   if (passwordsMatch) {
     // return the user object (without the password)
     delete user.password;
@@ -43,58 +44,26 @@ async function getUser({ username, password }) {
   }
 }
 
-//   const isValid = await bcrypt.compare(password, hashedPassword);
-//   try {
-//     const {
-//       rows: [user],
-//     } = await client.query(
-//       `SELECT *
-//         FROM users
-//         WHERE id= $1`
-//     );
-
-//     // if it doesn't exist (if there are no `rows` or `rows.length`), return null
-//     if (!isValid) {
-//       return null;
-//     }
-
-//     // if it does:
-//     // delete the 'password' key from the returned object
-//     delete user.password;
-
-//     // return the user object
-//     return user;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
 async function getUserById(userId) {
-  try {
-    const {
-      rows: [user],
-    } = await client.query(
-      `SELECT id, username
-        FROM users
-        WHERE id= $1`,
-      [userId]
-    );
+  const {
+    rows: [user],
+  } = await client.query(
+    `SELECT *
+    FROM users
+    WHERE id= $1`,
+    [userId]
+  );
 
-    // if it doesn't exist (if there are no `rows` or `rows.length`), return null
-    if (!user) {
-      return null;
-    }
-
-    // if it does:
-    // delete the 'password' key from the returned object
-    delete user.password;
-
-    // then add the posts to the user object with key 'posts'
-    // return the user object
-    return user;
-  } catch (error) {
-    throw error;
+  // if it doesn't exist (if there are no `rows` or `rows.length`), return null
+  if (!user) {
+    return null;
   }
+
+  // if it does, delete the 'password' key from the returned object
+  delete user.password;
+
+  // return the user object
+  return user;
 }
 
 async function getUserByUsername(username) {

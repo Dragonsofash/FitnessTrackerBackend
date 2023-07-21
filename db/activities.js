97c8275c2c60fd0ed68fddda024fee5f1d8bc2fb
Dragonsofash <agpaoajonathanb@gmail.com>
2023-07-20 20:50:41 -0400

    Started activities.js

--- db/activities.js
+++ db/activities.js
@@ -1,12 +1,39 @@
-const client = require('./client');
+const client = require("./client");
 
 // database functions
 async function createActivity({ name, description }) {
   // return the new activity
+  const {
+    rows: [activity],
+  } = await client.query(
+    `
+    INSERT INTO activities(name, description)
+    VALUES($1, $2)
+    RETURNING *;
+    `,
+    [name, description]
+  );
+
+  return activity;
 }
 
 async function getAllActivities() {
   // select and return an array of all activities
+  try {
+    const { rows: activityIds } = await client.query(`
+    SELECT id
+    FROM activities;
+  `);
+
+  const activities = await Promise.all(
+    activityIds.map((activity) => getActivityById(activity.id))
+  );
+
+  return activities;
+  } catch (error) {
+    throw
+  }
+  
 }
 
 async function getActivityById(id) {}
